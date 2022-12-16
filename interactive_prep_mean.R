b911c854afc17b5a1e47c4991cc27fb7a7816fac
katlass <lassiterkate@gmail.com>
2022-12-15 21:57:48 -0500

    final revisions

--- interactive_prep_mean.R
+++ interactive_prep_mean.R
@@ -8,6 +8,8 @@ dput(dataset2$zscore)
 
 #Getting y coordinates to place circles for each participant
 y=seq(5,555,length.out=nrow(dataset2))
+
+#Printing output as a list to paste into D3 file
 dput(y)
 
 #An alternate methodology was tested where the median value was the center of the interactive chart, and minmax scaling was used on either side to place circles. Please see interactive_prep_median.R
